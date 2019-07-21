8a757cc1b0de41ac948be4c6bbc7202e921eeb82
Leonid Rusakov <bengunn87@mail.ru>
2019-07-22 00:14:14 +0500

    Updates

---
+++ precache-manifest.7925cf794cfecde805fce2838cbbe39c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65bd3e1e716eb6cbb17f17bb2ecf7ddd",
+    "url": "/peolelist/index.html"
+  },
+  {
+    "revision": "8b93404decf863d2582f",
+    "url": "/peolelist/static/css/2.7c2c7a7e.chunk.css"
+  },
+  {
+    "revision": "efdd90b16666fad32ffb",
+    "url": "/peolelist/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "8b93404decf863d2582f",
+    "url": "/peolelist/static/js/2.eb47f6ba.chunk.js"
+  },
+  {
+    "revision": "efdd90b16666fad32ffb",
+    "url": "/peolelist/static/js/main.d66a117e.chunk.js"
+  },
+  {
+    "revision": "b7255e24295a6743cabe",
+    "url": "/peolelist/static/js/runtime~main.8ed22c05.js"
+  }
+]);
